c79db0c1383de9ebf09788d61a4b5b97ec2b77ee
wwwrwww65-netizen <wwwrwww65@gmail.com>
2026-09-23 07:32:17 +0300

    feat: add hotspot assets and configuration
    Initialize hotspot environment variables, add MikroTik portal files, and include Tajawal typography.

---
+++ config.js
@@ -0,0 +1,181 @@
+window.siteConfig = {
+    "siteName": "البدر نت",
+    "network-name": "البدر نت",
+    "subDecText": "لخدمات الإنترنت اللاسلكي",
+    "welcomeMessage": "أهلاً بك زائرنا في شبكة البدر نت",
+    "welcomeMessageV": true,
+    "supportPhone": "711720290",
+    "service-number": "711720290",
+    "callPhone": "711720290",
+    "whatsappPhone": "967711720290",
+    "whatsappMsg": "مرحبا خدمة عملاء شبكة البدر نت اللاسلكية",
+    "imageCount": "7",
+    "imageV": true,
+    "packages": [
+        {
+            "price": "100 ريال",
+            "time": "4 ساعات",
+            "size": "400 ميجا",
+            "transfer": "400 ميجا",
+            "validity": "4 يوم",
+            "vl": "4 يوم"
+        },
+        {
+            "price": "200 ريال",
+            "time": "12 ساعة",
+            "size": "700 ميجا",
+            "transfer": "700 ميجا",
+            "validity": "10 يوم",
+            "vl": "10 يوم"
+        },
+        {
+            "price": "250 ريال",
+            "time": "15 ساعة",
+            "size": "1 جيجا",
+            "transfer": "1 جيجا",
+            "validity": "15 يوم",
+            "vl": "15 يوم"
+        },
+        {
+            "price": "500 ريال",
+            "time": "50 ساعة",
+            "size": "2 جيجا",
+            "transfer": "2 جيجا",
+            "validity": "20 يوم",
+            "vl": "20 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "size": "25 جيجا",
+            "transfer": "25 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "size": "30 جيجا",
+            "transfer": "30 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "size": "50 جيجا",
+            "transfer": "50 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "size": "75 جيجا",
+            "transfer": "75 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "size": "100 جيجا",
+            "transfer": "100 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        }
+    ],
+    "profiles": [
+        {
+            "price": "100 ريال",
+            "time": "4 ساعات",
+            "transfer": "400 ميجا",
+            "validity": "4 يوم"
+        },
+        {
+            "price": "200 ريال",
+            "time": "12 ساعة",
+            "transfer": "700 ميجا",
+            "validity": "10 يوم"
+        },
+        {
+            "price": "250 ريال",
+            "time": "15 ساعة",
+            "transfer": "1 جيجا",
+            "validity": "15 يوم"
+        },
+        {
+            "price": "500 ريال",
+            "time": "50 ساعة",
+            "transfer": "2 جيجا",
+            "validity": "20 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "transfer": "25 جيجا",
+            "validity": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "transfer": "30 جيجا",
+            "validity": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "transfer": "50 جيجا",
+            "validity": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "transfer": "75 جيجا",
+            "validity": "30 يوم"
+        },
+        {
+            "price": "اشتراك شهري",
+            "time": "بدون تحديد",
+            "transfer": "100 جيجا",
+            "validity": "30 يوم"
+        }
+    ],
+    "offersTitle": "خدمات وعروض شبكة البدر نت 🔥",
+    "offers": "...:::: أهلاً بكم في شبكة البدر نت اللاسلكية تتميز شبكتنا بتقديم مزايا مختلفة لمشتركيها الأعزاء منها السرعة في التصفح ولعب الألعاب، استقرار وثبات الخدمة على مدار 24 ساعة ::::...",
+    "loan-button": true,
+    "loan-text": "أهلاً بك زائرنا في شبكة البدر نت",
+    "salesPoints": [
+        "المركز الرئيسي - شبكة البدر",
+        "البدر نت - العنوان: البدر - للتواصل: 711720290",
+        "موزع رسمي لشبكة البدر نت - للتواصل: 711720290"
+    ],
+    "sell-points": [
+        {
+            "name": "المركز الرئيسي - شبكة البدر"
+        },
+        {
+            "name": "البدر نت - العنوان: البدر - للتواصل: 711720290"
+        },
+        {
+            "name": "موزع رسمي لشبكة البدر نت - للتواصل: 711720290"
+        }
+    ],
+    "login-type": "passwordAsUser",
+    "enableHotCookie": true,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "input-autocomplete": "on",
+    "input-type": "text",
+    "input-rm-white-spaces": 1,
+    "input-to-arabic-numbers": 1,
+    "price-button": true,
+    "sell-point-button": true,
+    "show-date-field": true
+};
